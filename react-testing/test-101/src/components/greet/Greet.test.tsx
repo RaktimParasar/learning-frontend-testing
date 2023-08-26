404241617e367c3a8293ddfafed6ce47818d3f8b
Raktim Parasar <raktimparasar96@gmail.com>
2023-08-27 00:36:23 +0530

    code coverage

--- react-testing/test-101/src/components/greet/Greet.test.tsx
+++ react-testing/test-101/src/components/greet/Greet.test.tsx
@@ -8,12 +8,13 @@ describe("Greet", () => {
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
   });
-  describe("Nested", () => {
-    test("renders a name", () => {
-      render(<Greet name="Raktim" />);
-      const textPropElement = screen.getByText(/raktim/i);
-      expect(textPropElement).toBeInTheDocument();
-    });
+});
+
+describe("Nested", () => {
+  test("renders a name", () => {
+    render(<Greet name="Raktim" />);
+    const textPropElement = screen.getByText(/raktim/i);
+    expect(textPropElement).toBeInTheDocument();
   });
 });
 
